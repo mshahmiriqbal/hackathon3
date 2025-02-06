eea9263f348af0440bc9fd08f34e8047ba6878a1
mshahmiriqbal <mshahmiriqbal@gmail.com>
2025-02-06 14:44:52 +0500

    Update products.tsx

--- hj/src/app/pages/products.tsx
+++ hj/src/app/pages/products.tsx
@@ -5,8 +5,26 @@
 
 
 import { createClient } from 'next-sanity'
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-02-04'
 
-import { apiVersion, dataset, projectId } from '@/sanity/env'
+export const dataset = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+)
+
+export const projectId = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+)
+
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+  if (v === undefined) {
+    throw new Error(errorMessage)
+  }
+
+  return v
+}
 
 export const sanityClient1  = createClient({
   projectId,
